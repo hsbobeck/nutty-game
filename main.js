ba85da223ff0ce7c8b05536cc0ee5f04a03fcc0d
Henry Bobeck <hsbipod@gmail.com>
2014-08-09 17:10:31 -0400

    commented
    added comments to explain

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+/*-----------------------------------------------------------------------------------*/
+/*  This segment gives 1 nut every second
+/*-----------------------------------------------------------------------------------*/
 nutAmount = 0;
-
+// Adds 1 to the amount of nuts and updates the count displayed on screen every time it runs
 function increment() {
 	nutAmount += 1;
 	document.getElementById("nutAmount").innerHTML = nutAmount;
 }
 
+// run increment every second
 setInterval(increment, 1000)
+/*-----------------------------------------------------------------------------------*/
