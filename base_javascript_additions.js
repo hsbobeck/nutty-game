e93f575dc1bbe4e764ba3a4b8b04add544ce3367
Henry Bobeck <hsbipod@gmail.com>
2014-09-02 17:17:40 -0400

    Common scratch tickets now work successfully
    shiny and golden still have no reward, and the rewards for common are
    boring and need to be thought out

---
+++ base_javascript_additions.js
@@ -0,0 +1,22 @@
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+};
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
--- tickets.js
+++ tickets.js
@@ -6,7 +6,6 @@ var tickets = {
 	nbrOwnedGolden: 0,
 	priceForOne: 10,
 
-
 	// Functions
 	setNbrOwnedCommon: function(value) {
 		this.nbrOwnedCommon = value;
@@ -63,6 +62,88 @@ var tickets = {
 		htmlInteraction.setElementVisibility("commonCongrats", true)
 		htmlInteraction.setElementVisibility("shinyCongrats", false)
 		htmlInteraction.setElementVisibility("goldenCongrats", false)
+
+		commonOutcomes = ['2peanuts', '1peanut', '25nuts', '10nuts'];
+		uncommonOutcomes = ['5peanuts', '8peanuts', '65nuts', '80nuts'];
+		rareOutcomes = ['10peanuts', '15peanuts', '100nuts', '165nuts'];
+
+		shuffle(commonOutcomes);
+		shuffle(uncommonOutcomes);
+		shuffle(rareOutcomes);
+
+		defaultWarning = "Uh oh! Looks like something from the array doesn't have a case in the switch statements.";
+		random = Math.random();
+
+		if(random < 0.76) {
+			// these are common for this ticket
+			switch(commonOutcomes[0]) {
+				case '2peanuts':
+					peanuts.setNbrOwned(peanuts.nbrOwned + 2)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 2 peanuts.")
+					break;
+				case '1peanut':
+					peanuts.setNbrOwned(peanuts.nbrOwned + 1)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won a peanut.")
+					break;
+				case '25nuts':
+					nuts.setNbrOwned(nuts.nbrOwned + 25)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 25 nuts.")
+					break;
+				case '10nuts':
+					nuts.setNbrOwned(nuts.nbrOwned + 10)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 10 nuts.")
+					break;
+				default:
+					console.log(defaultWarning)
+					break;
+			}
+		} else if(random < 0.96) {
+			// these are uncommon for this ticket
+			switch(uncommonOutcomes[0]) {
+				case '5peanuts':
+					peanuts.setNbrOwned(peanuts.nbrOwned + 5)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 5 peanuts.")
+					break;
+				case '8peanuts':
+					peanuts.setNbrOwned(peanuts.nbrOwned + 8)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 8 peanuts.")
+					break;
+				case '65nuts':
+					nuts.setNbrOwned(nuts.nbrOwned + 65)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 65 nuts.")
+					break;
+				case '80nuts':
+					nuts.setNbrOwned(nuts.nbrOwned + 80)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 80 nuts.")
+					break;
+				default:
+					console.log(defaultWarning)
+					break;
+			}
+		} else {
+			// these are rare for this ticket
+			switch(rareOutcomes[0]) {
+				case '10peanuts':
+					peanuts.setNbrOwned(peanuts.nbrOwned + 10)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 10 peanuts.")
+					break;
+				case '15peanuts':
+					peanuts.setNbrOwned(peanuts.nbrOwned + 15)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 15 peanuts.")
+					break;
+				case '100nuts':
+					nuts.setNbrOwned(nuts.nbrOwned + 100)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 100 nuts.")
+					break;
+				case '165nuts':
+					nuts.setNbrOwned(nuts.nbrOwned + 165)
+					htmlInteraction.setInnerHtml("commonCongrats", "Congratulations! You won 165 nuts.")
+					break;
+				default:
+					console.log(defaultWarning)
+					break;
+			}
+		}
 	},
 
 	scratchShiny: function() {
@@ -70,6 +151,18 @@ var tickets = {
 		htmlInteraction.setElementVisibility("shinyCongrats", true)
 		htmlInteraction.setElementVisibility("commonCongrats", false)
 		htmlInteraction.setElementVisibility("goldenCongrats", false)
+
+		random = Math.random();
+		if(random < 0.76) {
+			// these are common for this ticket
+
+		} else if(random < 0.96) {
+			// these are uncommon for this ticket
+
+		} else {
+			// these are rare for this ticket
+
+		}
 	},
 
 	scratchGolden: function() {
@@ -77,6 +170,18 @@ var tickets = {
 		htmlInteraction.setElementVisibility("goldenCongrats", true)
 		htmlInteraction.setElementVisibility("commonCongrats", false)
 		htmlInteraction.setElementVisibility("shinyCongrats", false)
+
+		random = Math.random();
+		if(random < 0.76) {
+			// these are common for this ticket
+
+		} else if(random < 0.96) {
+			// these are uncommon for this ticket
+
+		} else {
+			// these are rare for this ticket
+
+		}
 	},
 
 };
